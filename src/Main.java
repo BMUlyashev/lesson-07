e5f476a12e569bf89aa2089f6890e496db8d458a
b.ulyashev <b.m.ulyashev@mail.ru>
2022-06-01 20:34:43 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+    }
 
+    private static void task1() {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+    }
+
+    private static void task2() {
+        String fullName = "Ivanov Ivan Ivanovich";
+        fullName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName);
+    }
+
+    private static void task3() {
+        String fullName = "Иванов Семён Семёнович";
+        String[] words = fullName.split(" ");
+        for (int i = 0; i <  words.length; i++) {
+            if (words[i].contains("ё")) {
+                words[i] = words[i].replace("ё", "е");
+            }
+        }
+        fullName = words[0] + " " + words[1] + " " + words[2];
+        System.out.println("Данные ФИО сотрудника — " + fullName);
     }
 }
